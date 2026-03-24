d7605a858604df8cb488f3720f3d807d1c4ffe07
Adrik-Janeczko-wvu <adj00012@mix.wvu.edu>
2026-03-24 18:39:43 -0400

    Created with statements for each of the quality checks used for the dataset

---
+++ data_quality.sql
@@ -0,0 +1,23 @@
+-- Creating  a table using with statements to create different categories. 
+-- Table counts counts the customers and returns a number value for the amount of customers. 
+WITH table_counts AS (
+    SELECT 'customers' AS metric, COUNT(*) AS value FROM customers
+    UNION ALL
+    SELECT 'orders', COUNT(*) FROM orders
+),
+-- This checks all of the nulls within the data and returns the amount of nulls
+null_checks AS (
+    SELECT 
+        'null_customer_id' AS metric,
+        SUM(CASE WHEN customer_id IS NULL THEN 1 ELSE 0 END) AS value
+    FROM orders
+),
+-- orphaned orders counts the amount of customer_ids that have ni origin key to them. 
+orphaned_orders AS (
+    SELECT 
+        'orphan_orders' AS metric,
+        COUNT(*) AS value
+    FROM orders o
+    LEFT JOIN customers c ON o.customer_id = c.customer_id
+    WHERE c.customer_id IS NULL
+)
