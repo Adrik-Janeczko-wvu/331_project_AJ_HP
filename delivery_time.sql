75e746052bdaf0ca2edc0774351f0491692b211a
Adrik-Janeczko-wvu <adj00012@mix.wvu.edu>
2026-03-24 21:31:30 -0400

    Update delivery_time.sql
    Completed aggregate CTE

--- delivery_time.sql
+++ delivery_time.sql
@@ -1,4 +1,7 @@
- 
+-- Business Question:
+-- Which states experience the worst delivery delays?
+-- Approach: 
+-- Create two CTEs one for delivery time and one for aggregate. Using delivery times create the aggregate and show the final tabele and order by delay rank 
 WITH delivery_times AS (
     SELECT 
         c.customer_state,
@@ -7,4 +10,15 @@ WITH delivery_times AS (
     FROM orders as o
     JOIN customers as c ON o.customer_id = c.customer_id
     WHERE o.order_delivered_customer_date IS NOT NULL -- makes sure there is an order date 
+),
+
+aggregated AS (
+    SELECT 
+        customer_state,
+        AVG(actual_days) AS avg_actual,
+        AVG(estimated_days) AS avg_estimated,
+        AVG(actual_days - estimated_days) AS delay -- cerates the aggregate used in the final table
+    FROM delivery_times
+    GROUP BY customer_state
 )
+
