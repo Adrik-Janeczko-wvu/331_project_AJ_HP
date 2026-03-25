e01c49ffeb23b0fa168700ac922e9045aa86cc16
Adrik-Janeczko-wvu <adj00012@mix.wvu.edu>
2026-03-24 21:26:10 -0400

    Update delivery_time.sql
    Create first CTE for delivery times using a join table with orders and customers

--- delivery_time.sql
+++ delivery_time.sql
@@ -1,1 +1,10 @@
--- create sql file
+ 
+WITH delivery_times AS (
+    SELECT 
+        c.customer_state,
+        DATE_DIFF('day', o.order_purchase_timestamp, o.order_delivered_customer_date) AS actual_days,
+        DATE_DIFF('day', o.order_purchase_timestamp, o.order_estimated_delivery_date) AS estimated_days
+    FROM orders as o
+    JOIN customers as c ON o.customer_id = c.customer_id
+    WHERE o.order_delivered_customer_date IS NOT NULL -- makes sure there is an order date 
+)
