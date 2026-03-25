e97464ede2c2b8bdcaa0b9cf6a24bdc8e4d8ab0f
Adrik-Janeczko-wvu <adj00012@mix.wvu.edu>
2026-03-24 21:01:22 -0400

    Update seller_scorecard.sql
    Created final two CTEs fixing order revies to make one review per order. created another CTE from the review value themselves using the previously fixed order review. Joined all CTEs together onto one table.

--- seller_scorecard.sql
+++ seller_scorecard.sql
@@ -1,3 +1,7 @@
+-- Business Question:
+-- Which sellers perform best based on revenue, delivery speed, and reviews?
+-- Approach:
+-- Complete each given metric using CTEs and the rank by revenue
 WITH revenue AS (
     SELECT 
         seller_id,
@@ -13,5 +17,33 @@ delivery AS (
     FROM orders as o
     JOIN order_items as oi USING (order_id)
     WHERE o.order_delivered_customer_date IS NOT NULL
-    GROUP BY oi.seller_id -- 
+    GROUP BY oi.seller_id  
+), 
+-- fixing duplication one review per order
+order_reviews_clean AS (
+    SELECT 
+        order_id,
+        AVG(review_score) AS review_score
+    FROM order_reviews
+    GROUP BY order_id
+),
+-- create review CTE to show average review scores jined with seller id 
+reviews AS (
+    SELECT 
+        oi.seller_id,
+        AVG(r.review_score) AS avg_review_score
+    FROM order_reviews_clean as r
+    JOIN order_items as oi USING (order_id)
+    GROUP BY oi.seller_id
 )
+-- final select for the final table. joining all three ctes onto one table and using rank feature
+SELECT 
+    r.seller_id,
+    r.total_revenue,
+    d.avg_delivery_days,
+    rv.avg_review_score,
+    RANK() OVER (ORDER BY r.total_revenue DESC) AS revenue_rank
+FROM revenue as r
+LEFT JOIN delivery as d USING (seller_id)
+LEFT JOIN reviews as rv USING (seller_id)
+ORDER BY revenue_rank;
